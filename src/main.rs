6e843a2bded303d68660ba7bcf70a5ad06244c58
impulseweb3 <impulseweb3@gmail.com>
2026-03-17 22:07:55 +0100

    Add transaction filtering based on value using configured target

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use crate::config::get_config;
 use crate::providers::{get_http_provider, get_ws_provider};
+use alloy::consensus::Transaction;
+use alloy::primitives::U256;
 use alloy::providers::Provider;
 use futures_util::StreamExt;
+use std::ops::Mul;
 
 mod config;
 mod providers;
@@ -9,6 +12,9 @@ mod providers;
 #[tokio::main]
 async fn main() {
     let config = get_config();
+
+    let target_value = U256::from(config.target.value);
+    let target_value = target_value.mul(U256::from(10).pow(U256::from(18)));
 
     let ws_provider = get_ws_provider(&config.ws_url).await;
     let http_provider = get_http_provider(&config.http_url);
@@ -22,6 +28,11 @@ async fn main() {
 
     while let Some(block) = stream.next().await {
         let block = block.unwrap();
-        println!("{:#?}", block);
+
+        for transaction in block.into_transactions_iter() {
+            if transaction.value() > target_value {
+                println!("{:#?}", transaction);
+            }
+        }
     }
 }
