8d6129369acf4b27f9f3bccf9900f1ea81de2f12
impulseweb3 <impulseweb3@gmail.com>
2026-03-17 19:24:52 +0100

    Replace main function to use async runtime with WebSocket and HTTP providers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use crate::config::get_config;
+use crate::providers::{get_http_provider, get_ws_provider};
+use alloy::providers::Provider;
+use futures_util::StreamExt;
+
+mod config;
+mod providers;
+
+#[tokio::main]
+async fn main() {
+    let config = get_config();
+
+    let ws_provider = get_ws_provider(&config.ws_url).await;
+    let http_provider = get_http_provider(&config.http_url);
+
+    let mut stream = ws_provider
+        .subscribe_full_blocks()
+        .into_stream()
+        .await
+        .unwrap();
+
+    while let Some(block) = stream.next().await {
+        println!("{}", block.unwrap().header.number);
+    }
 }
