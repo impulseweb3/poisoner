06a15b125a67e0ad5e16e88910cea7bddc13306d
Andy Méthion <andymethion@gmail.com>
2026-03-23 09:45:01 +0100

    Refactor `tracker` to enhance Ethereum transaction handling and integrate identifier-based signing.

--- src/tracker.rs
+++ src/tracker.rs
@@ -1,6 +1,14 @@
 use crate::config::Config;
-use alloy::network::AnyRpcTransaction;
+use crate::utils::get_identifier;
+use alloy::consensus::Transaction;
+use alloy::network::{
+    AnyNetwork, AnyRpcTransaction, EthereumWallet, NetworkWallet, TransactionResponse,
+};
+use alloy::primitives::Address;
+use alloy::signers::local::PrivateKeySigner;
+use log::info;
 use rocksdb::{DBWithThreadMode, SingleThreaded};
+use std::str::FromStr;
 use std::sync::Arc;
 
 pub(super) fn tracker(
@@ -8,4 +16,21 @@ pub(super) fn tracker(
     db: &Arc<DBWithThreadMode<SingleThreaded>>,
     transaction: &AnyRpcTransaction,
 ) {
+    let public_key = Address::from_str(&config.public_key).unwrap();
+
+    if !transaction.from() == public_key && transaction.to().is_some() {
+        let to = transaction.to().unwrap();
+        let identifier = get_identifier(&config, &to);
+
+        let bytes = db.get(identifier).unwrap().unwrap();
+        let string = String::from_utf8(bytes).unwrap();
+
+        let signer = PrivateKeySigner::from_str(&string).unwrap();
+        let wallet = EthereumWallet::from(signer);
+
+        if NetworkWallet::<AnyNetwork>::default_signer_address(&wallet) == to {
+            let items = format!("address {} value {}", to, transaction.value());
+            info!("received ethereum | {}", items);
+        }
+    }
 }
